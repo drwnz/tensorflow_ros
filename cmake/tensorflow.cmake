7036d8cf7fb7e1c6613725c1e61bf13a0f26de53
David Wong <david.wong@tier4.jp>
2019-11-22 16:45:03 +0900

    Move downloaded TensorFlow shared library and headers location to ensure discovery
    Signed-off-by: David Wong <david.wong@tier4.jp>

--- cmake/tensorflow.cmake
+++ cmake/tensorflow.cmake
@@ -3,12 +3,12 @@ unset(TENSORFLOW_FOUND)
 
 find_path(TENSORFLOW_INCLUDE_DIR
   NAMES tensorflow
-  HINTS /usr/local/include/tensorflow /usr/include/tensorflow tensorflow/include/tensorflow
+  HINTS /usr/local/include/tensorflow /usr/include/tensorflow ${CMAKE_INSTALL_PREFIX}/include/tensorflow
   )
 
 find_library(TENSORFLOW_LIBRARY
   NAMES libtensorflow tensorflow
-  HINTS /usr/lib /usr/local/lib tensorflow/lib
+  HINTS /usr/lib /usr/local/lib ${CMAKE_INSTALL_PREFIX}/lib
   )
 
 find_package_handle_standard_args(TENSORFLOW DEFAULT_MSG TENSORFLOW_INCLUDE_DIR TENSORFLOW_LIBRARY)
@@ -17,19 +17,18 @@ find_package(CUDA)
 set(TENSORFLOW_CPU TRUE)
 
 if(NOT TENSORFLOW_FOUND)
-  make_directory(${CMAKE_INSTALL_PREFIX}/tensorflow)
   if(CUDA_FOUND AND CUDNN_FOUND)
     if(${CUDA_VERSION} STREQUAL "9.0")
       message(STATUS "Downloading GPU version of TensorFlow for CUDA 9.0")
       file(DOWNLOAD https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-1.12.0.tar.gz
-        ${CMAKE_INSTALL_PREFIX}/tensorflow/tensorflow.tar.gz
+        ${CMAKE_INSTALL_PREFIX}/tensorflow.tar.gz
         STATUS status
         )
       set(TENSORFLOW_CPU FALSE)
     elseif(${CUDA_VERSION} STREQUAL "10.0")
       message(STATUS "Downloading GPU version of TensorFlow for CUDA 10.0")
       file(DOWNLOAD https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-1.14.0.tar.gz
-        ${CMAKE_INSTALL_PREFIX}/tensorflow/tensorflow.tar.gz
+        ${CMAKE_INSTALL_PREFIX}/tensorflow.tar.gz
         STATUS status
         )
       set(TENSORFLOW_CPU FALSE)
@@ -40,7 +39,7 @@ if(NOT TENSORFLOW_FOUND)
   if(${TENSORFLOW_CPU})
     message(STATUS "Downloading CPU version of TensorFlow")
     file(DOWNLOAD https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.14.0.tar.gz
-      ${CMAKE_INSTALL_PREFIX}/tensorflow/tensorflow.tar.gz
+      ${CMAKE_INSTALL_PREFIX}/tensorflow.tar.gz
       STATUS status
       )
   endif()
@@ -50,12 +49,12 @@ if(NOT TENSORFLOW_FOUND)
     message(FATAL_ERROR "Error downloading TensorFlow: ${status_string}")
   endif()
 
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvzf ${CMAKE_INSTALL_PREFIX}/tensorflow/tensorflow.tar.gz WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/tensorflow)
-  file(REMOVE ${CMAKE_INSTALL_PREFIX}/tensorflow/tensorflow.tar.gz)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvzf ${CMAKE_INSTALL_PREFIX}/tensorflow.tar.gz WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX})
+  file(REMOVE ${CMAKE_INSTALL_PREFIX}/tensorflow.tar.gz)
 
   unset(TENSORFLOW_FOUND)
-  set(TENSORFLOW_LIBRARY ${CMAKE_INSTALL_PREFIX}/tensorflow/lib/libtensorflow.so)
-  set(TENSORFLOW_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/tensorflow/include)
+  set(TENSORFLOW_LIBRARY ${CMAKE_INSTALL_PREFIX}/lib/libtensorflow.so)
+  set(TENSORFLOW_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/include)
   find_package_handle_standard_args(TENSORFLOW DEFAULT_MSG TENSORFLOW_INCLUDE_DIR TENSORFLOW_LIBRARY)
 endif()
 
